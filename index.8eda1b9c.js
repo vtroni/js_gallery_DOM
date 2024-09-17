7fd990e45bf6c54fa6e8f2c6dda89354f1cd5ec1
Virineya Marchuk <marchukvirineya@gmail.com>
2024-09-17 14:31:17 +0300

    make build

---
+++ index.8eda1b9c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");r&&(t.src=r.href)});
+//# sourceMappingURL=index.8eda1b9c.js.map
